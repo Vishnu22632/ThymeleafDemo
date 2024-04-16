5f778c8002e2f628dcf4e639411fea474c134915
Vishnu <vishnu22632@gmail.com>
2024-04-16 20:17:55 +0545

    dynamic value passed

---
+++ src/main/java/com/springsecurity/controller/MyController.java
@@ -0,0 +1,55 @@
+package com.springsecurity.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("name", "Vishnu Thakur");
+		model.addAttribute("date", new Date().toLocaleString());
+		
+		return "about";
+	}
+	
+	@GetMapping("/condition")
+	public String conditionHandler(Model m) {
+		System.out.println("Conditional handler executed...");
+		m.addAttribute("isActive", true);
+		
+		m.addAttribute("gender", "M");
+		
+		List<Integer> list=List.of(12);
+		
+		m.addAttribute("myList", list);
+		
+		return "condition";
+	}
+	
+	@GetMapping("/service")
+	public String service(Model m) {
+		
+		
+		return "service";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
